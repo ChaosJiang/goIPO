18c9bde41fbc54a71d0afe2f388ff2664ec13e50
[dayDreamer] <[codemelody@163.com]>
2021-03-21 00:38:43 +0900

    get ipo nodes

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"time"
 
+	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 	"github.com/joho/godotenv"
 )
@@ -20,55 +22,73 @@ func getEnvVariable(key string) string {
 }
 
 func main() {
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", false),
-	)
-	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	// opts := append(chromedp.DefaultExecAllocatorOptions[:],
+	// 	chromedp.Flag("headless", false),
+	// )
+	// ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	// defer cancel()
+
+	// ctx, cancel = chromedp.NewContext(ctx)
+	// defer cancel()
+
+	// create headless chrome instance
+	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
-	// create chrome instance
-	ctx, cancel = chromedp.NewContext(ctx,
-		// 设置日志方法
-		chromedp.WithLogf(log.Printf),)
-
 	// create a timeout
-	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
-
+	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
 	// 希望浏览器关闭，使用cancel()方法即可
 	defer cancel()
 
-	username := getEnvVariable("USERNAME")
-	password := getEnvVariable("PASSWD")
-	loginUrl := getEnvVariable("LOGIN_URL")
-
-	err := chromedp.Run(ctx,
-		login(loginUrl, username, password),
-		ipoPage(),
-	)
+	// login
+	err :=login(ctx)
 	if err != nil {
 		log.Fatal(err)
-	}	
+	}
+
+	// get ipo links nodes
+	joinNodes, err := ipoPage(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, node := range joinNodes {
+		fmt.Println(node.AttributeValue("href"))
+	}
 }
 
 // Login in tasks
-func login(urlstr string, username string, password string) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(`https://www.rakuten-sec.co.jp`),
+func login(ctx context.Context) error {
+	username := getEnvVariable("USERNAME")
+	password := getEnvVariable("PASSWD")
+	loginUrl := getEnvVariable("LOGIN_URL")
+	// login 
+	if err := chromedp.Run(
+		ctx,
+		chromedp.Navigate(loginUrl),
 		// wait for login form element is visible (ie, page is loaded)
 		chromedp.WaitVisible(`#form-login-id`, chromedp.ByID),
 		// fill in username and password
 		chromedp.SetValue(`#form-login-id`, username, chromedp.ByID),
 		chromedp.SetValue(`#form-login-pass`, password, chromedp.ByID),
 		// click login button
-		chromedp.Click(`button[class="s3-form-login__btn"]`, chromedp.ByQuery),
+		chromedp.Click(`button.s3-form-login__btn`, chromedp.ByQuery),
+	); err != nil {
+		return fmt.Errorf("could not login %s", err)
 	}
+	return nil
 }
 
-func ipoPage() chromedp.Tasks {
-	return chromedp.Tasks{
+func ipoPage(ctx context.Context, ) ([]*cdp.Node, error) {
+	var nodes []*cdp.Node
+	// navigate to ipo page
+	if err := chromedp.Run(
+		ctx,
 		chromedp.WaitVisible(`#gmenu_domestic_stock`, chromedp.ByID),
 		chromedp.Click(`#gmenu_domestic_stock`, chromedp.ByID),
-		chromedp.Click(`ul[class="pcm-nav-03__list"] > li:nth-child(3)`, chromedp.ByQuery),
-		chromedp.Sleep(20 * time.Second),
+		chromedp.Click(`ul.pcm-nav-03__list > li:nth-child(3)`, chromedp.ByQuery),
+		chromedp.Nodes(`//div/nobr/a[contains(text(), "参加")]`, &nodes, chromedp.BySearch),
+	); err != nil {
+		return nil, fmt.Errorf("could not login %s", err)
 	}
+	return nodes, nil;
 }
