ac4d35cdfb01548d7d565c3d15b73fefeea46747
[dayDreamer] <[codemelody@163.com]>
2021-05-27 08:11:25 +0900

    dev version

--- main.go
+++ main.go
@@ -22,21 +22,21 @@ func getEnvVariable(key string) string {
 }
 
 func main() {
-	// opts := append(chromedp.DefaultExecAllocatorOptions[:],
-	// 	chromedp.Flag("headless", false),
-	// )
-	// ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	// defer cancel()
-
-	// ctx, cancel = chromedp.NewContext(ctx)
-	// defer cancel()
-
-	// create headless chrome instance
-	ctx, cancel := chromedp.NewContext(context.Background())
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", false),
+	)
+	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancel()
 
+	ctx, cancel = chromedp.NewContext(ctx)
+	defer cancel()
+
+	// // create headless chrome instance
+	// ctx, cancel := chromedp.NewContext(context.Background())
+	// defer cancel()
+
 	// create a timeout
-	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+	ctx, cancel = context.WithTimeout(ctx, 60*time.Second)
 	// 希望浏览器关闭，使用cancel()方法即可
 	defer cancel()
 
@@ -46,14 +46,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// get ipo links nodes
-	joinNodes, err := ipoPage(ctx)
+	// get ipo participation links nodes
+	joinNodes, applyNodes, err := getIpoNodes(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, node := range joinNodes {
-		fmt.Println(node.AttributeValue("href"))
+	// do IPO
+	err = doJoin(ctx, joinNodes)
+	if err != nil {
+		log.Fatal(err)
 	}
+	err = doApply(ctx, applyNodes)
 }
 
 // Login in tasks
@@ -73,22 +76,64 @@ func login(ctx context.Context) error {
 		// click login button
 		chromedp.Click(`button.s3-form-login__btn`, chromedp.ByQuery),
 	); err != nil {
-		return fmt.Errorf("could not login %s", err)
+		return fmt.Errorf("could not login: %s", err)
 	}
 	return nil
 }
 
-func ipoPage(ctx context.Context, ) ([]*cdp.Node, error) {
-	var nodes []*cdp.Node
+func getIpoNodes(ctx context.Context) ([]*cdp.Node, []*cdp.Node, error) {
+	var joinNodes []*cdp.Node
+	var applyNodes []*cdp.Node
+	var ipoUrl string
 	// navigate to ipo page
 	if err := chromedp.Run(
 		ctx,
 		chromedp.WaitVisible(`#gmenu_domestic_stock`, chromedp.ByID),
 		chromedp.Click(`#gmenu_domestic_stock`, chromedp.ByID),
 		chromedp.Click(`ul.pcm-nav-03__list > li:nth-child(3)`, chromedp.ByQuery),
-		chromedp.Nodes(`//div/nobr/a[contains(text(), "参加")]`, &nodes, chromedp.BySearch),
+		chromedp.Nodes(`//div/nobr/a[contains(text(), "参加")]`, &joinNodes, chromedp.BySearch),
+		chromedp.Nodes(`//div/a[contains(text(), "申込")]`, &applyNodes, chromedp.BySearch),
+		chromedp.Location(&ipoUrl),
 	); err != nil {
-		return nil, fmt.Errorf("could not login %s", err)
+		return nil, nil, fmt.Errorf("could not get IPO nodes: %s", err)
 	}
-	return nodes, nil;
+	return joinNodes,applyNodes, nil;
+}
+
+func doJoin(ctx context.Context, nodes []*cdp.Node) error {
+	// force max timeout of 15 seconds for retrieving and processing the data
+	shortPassword := getEnvVariable("SHORT_PASSWD")
+	// iterate nodes
+	for _, node := range nodes {
+		if err := chromedp.Run(
+			ctx,
+			chromedp.MouseClickNode(node),
+			chromedp.WaitVisible(`//input[@type='button' and contains(@value, "同意する") ]`, chromedp.BySearch),
+			chromedp.Click(`//input[@type='button' and contains(@value, "同意する") ]`, chromedp.BySearch),
+			// fill in ipo num (defalut value is 100)
+			chromedp.WaitVisible(`//input[@type='submit' and contains(@value, "確　認") ]`, chromedp.BySearch),
+			chromedp.SetValue(`//input[@type='text' and @name='value']`, "100", chromedp.BySearch),
+			chromedp.SetValue(`#price`, "0", chromedp.ByID),
+			chromedp.Click(`//input[@type='submit' and contains(@value, "確　認")]`, chromedp.BySearch),
+			chromedp.Sleep(2*time.Second),
+			// file in confirm passwd
+			chromedp.WaitVisible(`//input[@type='submit' and contains(@value, "参加申込")]`, chromedp.BySearch),
+			chromedp.SetValue(`//input[@type='password' and @name='password']`, shortPassword, chromedp.BySearch),
+			chromedp.Click(`//input[@type='submit' and contains(@value, "参加申込")]`, chromedp.BySearch),
+			// navigate back 3 times
+			chromedp.WaitVisible(`//font[@class='ord_subtitle-01' and contains(text(), "受付完了") ]`, chromedp.BySearch),
+			chromedp.NavigateBack(),
+			chromedp.NavigateBack(),
+			chromedp.NavigateBack(),
+			chromedp.NavigateBack(),
+		); err != nil {
+			return fmt.Errorf("could not do ipo")
+		}
+	}
+	return nil
+}
+
+
+func doApply(ctx context.Context, nodes []*cdp.Node) error {
+	return nil
 }
